edc337f63a5c31ccf867a1b8118a30c4d63ed2d5
Alexandre Konioukhov <alexan1@gmail.com>
2019-06-17 14:25:40 -0400

    pakert

---
+++ WikiDataProvider/WikiDataProvider.fs
@@ -0,0 +1,12 @@
+﻿namespace FSharp.Data.WikiDataProvider
+
+open System
+open System.Reflection
+open FSharp.Core.CompilerServices
+open FSharp.Quotations
+open ProviderImplementation.ProvidedTypes
+
+
+[<TypeProvider>]
+type WikiDataTypeProvider(config: TypeProviderConfig) as this =
+    inherit TypeProviderForNamespaces(config, assemblyReplacementMap=[ "FSharp.Data.DesignTime", "FSharp.Data" ])
